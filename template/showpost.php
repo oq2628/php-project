8f218f83efbfe02e703187d88d0ee1aa516eb8ab
oq2628 <quann260696@gmail.com>
2020-12-16 09:50:32 +0700

    del 2.css

--- template/showpost.php
+++ template/showpost.php
@@ -6,7 +6,6 @@
     <title>Edit</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- <link rel="stylesheet" href="./public/css/form.css"> -->
 </head>
 <body>
 <?php foreach($getbyids as $getbyid) : ?>
